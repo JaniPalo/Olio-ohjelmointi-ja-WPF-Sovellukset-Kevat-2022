a0ee8b8238bf35dab2773e21577f6a83c9419d97
Jani Palo <jani.palo@student.careeria.fi>
2022-01-10 14:34:31 +0200

    KiuasTehtävä3

---
+++ OlioOhjelmointi/Harjoitus3/Kiuas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus3
+{
+    class Kiuas
+    {
+        private string Nimi;
+        private int Lämpotila;
+        private int Kosteus;
+        public bool Tila = false;
+
+        public Kiuas(string _nimi, int _lämpötila, int _kosteus)
+        {
+            Nimi = _nimi;
+            Lämpotila = _lämpötila;
+            Kosteus = _kosteus;
+
+        }
+        public void NäytäTiedot()
+        {
+            if (Tila)
+            {
+                Console.WriteLine("Kiuas" + Nimi + " on päällä");
+                Console.WriteLine("Lämpötila = " + Lämpotila);
+                Console.WriteLine("Kosteus = " + Kosteus);
+            }
+            else
+            {
+                Console.WriteLine("Kiuas" + " ei ole päällä");
+            }
+        }
+        public void SäädäLämpötila(int uusiLämpötila)
+        {
+            Lämpotila = uusiLämpötila;
+        }
+    }
+}
---
+++ OlioOhjelmointi/Harjoitus3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Harjoitus3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas kiuas1 = new Kiuas( "Kiuas", 90, 20);
+            kiuas1.NäytäTiedot();
+            kiuas1.Tila = true;
+            kiuas1.NäytäTiedot();
+            
+        }
+    }
+}
