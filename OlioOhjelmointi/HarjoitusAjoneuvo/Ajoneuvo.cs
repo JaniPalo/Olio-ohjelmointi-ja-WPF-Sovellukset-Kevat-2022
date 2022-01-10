5f97ddd5690a7e001e1d1ce5108943cf51c16800
Jani Palo <jani.palo@student.careeria.fi>
2022-01-10 13:36:54 +0200

    AjoneuvoHarjoitus

---
+++ OlioOhjelmointi/HarjoitusAjoneuvo/Ajoneuvo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HarjoitusAjoneuvo
+{
+    class Ajoneuvo
+    {
+        public string Nimi;
+        public int Nopeus;
+        public int Renkaat;
+
+
+        public void DataTulostus()
+        {
+            Console.WriteLine(" Ajoneuvon malli: " + Nimi);
+            Console.WriteLine(" Ajoneuvon nopeus: " + Nopeus);
+            Console.WriteLine( " Ajoneuvon Renkaat:" + Renkaat);
+        }
+    }
+}
---
+++ OlioOhjelmointi/HarjoitusAjoneuvo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HarjoitusAjoneuvo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ajoneuvo Auto1 = new Ajoneuvo();
+            Auto1.Nimi = "Opel";
+            Auto1.Renkaat = 4;
+            Auto1.Nopeus = 120;
+
+            Auto1.DataTulostus();
+
+            Ajoneuvo Mopo = new Ajoneuvo();
+            Mopo.Nimi = "Yamaha";
+            Mopo.Renkaat = 2;
+            Mopo.Nopeus = 70;
+
+            Mopo.DataTulostus();
+
+
+        }
+    }
+}
