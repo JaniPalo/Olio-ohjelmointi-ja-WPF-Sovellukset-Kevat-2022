2d5fd60d90c821ae34d53654b6db6a8189ea5729
Jani Palo <jani.palo@student.careeria.fi>
2022-01-10 14:06:40 +0200

    harjoitus3

---
+++ OlioOhjelmointi/Harjoitus2/Opiskelija.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus2
+{
+    class Opiskelija
+    {
+        private string Nimi;
+        private string OpiskelijaID;
+        private int Opintopisteet;
+
+        public  Opiskelija(string _nimi, string _OpiskelijaID, int _Opintopisteet)
+        {
+            Nimi = _nimi;
+            OpiskelijaID = _OpiskelijaID;
+            Opintopisteet = _Opintopisteet;
+
+        }
+        public void TulostaData()
+        {
+            Console.WriteLine(Nimi + " ID: " + OpiskelijaID + " Opintopisteet: " + Opintopisteet);
+        }
+
+        public void MuokkaaOpintoPisteitä(int määrä)
+        {
+            Opintopisteet += määrä;
+        }
+    }
+}
---
+++ OlioOhjelmointi/Harjoitus2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Harjoitus2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija opiskelija1 = new Opiskelija("Pekka", " 1929" ,0);
+            opiskelija1.TulostaData();
+            opiskelija1.MuokkaaOpintoPisteitä(15);
+            opiskelija1.MuokkaaOpintoPisteitä(20);
+            opiskelija1.TulostaData();
+
+        }
+    }
+}
