e34362caf5ccde09e88c7d4e691ba6eed58387c7
Jani Palo <jani.palo@student.careeria.fi>
2022-01-10 13:17:58 +0200

    Koirankonstruktori

--- OlioOhjelmointi/TestiApp/Koira.cs
+++ OlioOhjelmointi/TestiApp/Koira.cs
@@ -10,6 +10,17 @@ namespace TestiApp
         public int ikä;
         public string rotu;
 
+        public Koira() { }
+
+        public Koira(string _nimi, int _ikä, string _rotu)
+        {
+            nimi = _nimi;
+            ikä = _ikä;
+            rotu = _rotu;
+
+            Hauku();
+        }
+
         public void TulostaData()
         {
             Console.WriteLine(" Koiran nimi: " + nimi + " Ikä: " + ikä + " Rotu: " + rotu);
