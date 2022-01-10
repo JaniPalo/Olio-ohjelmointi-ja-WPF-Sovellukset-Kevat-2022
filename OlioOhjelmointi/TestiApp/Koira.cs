c71bcd34c7b8fe56dfdfb839b8bfc20699406d39
Jani Palo <jani.palo@student.careeria.fi>
2022-01-10 12:59:00 +0200

    KoiraEsimerkki

---
+++ OlioOhjelmointi/TestiApp/Koira.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestiApp
+{
+    class Koira
+    {
+        public string nimi;
+        public int ikä;
+        public string rotu;
+
+        public void TulostaData()
+        {
+            Console.WriteLine(" Koiran nimi: " + nimi + " Ikä: " + ikä + " Rotu: " + rotu);
+        }
+
+        public void Hauku()
+        {
+            Console.WriteLine("Woof Woof ");
+        }
+    }
+}
--- OlioOhjelmointi/TestiApp/Program.cs
+++ OlioOhjelmointi/TestiApp/Program.cs
@@ -4,9 +4,24 @@ namespace TestiApp
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Koira koira1 = new Koira();
+            koira1.nimi = "Rekku";
+            koira1.ikä = 4;
+            koira1.rotu = " Saksanpaimenkoira";
+
+            koira1.TulostaData();
+            koira1.Hauku();
+
+            Koira koira2 = new Koira();
+            koira2.nimi = "Pertti";
+            koira2.ikä = 2;
+            koira2.rotu = " Mopsi";
+
+            koira2.TulostaData();
+            
         }
     }
 }
