918813b58d70ff1a9f665bfa45f8d5efe824e049
Jani Palo <jani.palo@student.careeria.fi>
2022-01-11 11:58:04 +0200

    tehtäväPerintä

---
+++ OlioOhjelmointi/perintä/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Perintä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Henkilö henkilö = new Henkilö("Matti", 20, 1042104); // Henkilö olio. Henkilö EI ole Opiskelija
+            Opiskelija opiskelija = new Opiskelija("Jorma", 18, 2155821, 12345, "Careeria"); // Opiskelija olio. Opiskelija on Henkilö
+
+            henkilö.TulostaTiedot();
+            opiskelija.TulostaTiedot();
+
+            henkilö.Syö();
+            henkilö.Juo();
+
+            Console.WriteLine();
+
+            opiskelija.Opiskele();
+            opiskelija.Juo();
+           
+        }
+    }
+}
